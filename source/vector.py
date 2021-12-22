ca03192da3130c663bce5793851d262042abde17
Dave Amiana <amianism25@gmail.com>
2021-12-22 15:12:52 +0800

    updated documentation with code examples

--- source/vector.py
+++ source/vector.py
@@ -1,11 +1,44 @@
 from __future__ import annotations
-from typing import List, Union
+from typing import List, Union, Callable
 from numbers import Real
 from math import sqrt, acos
 
 class vector:
     """
-    Representation of real-valued vector :math:`\\vec{v} \\in \\mathbb{R}^n`
+    Representation of real-valued vector :math:`\\vec{v} \\in \\mathbb{R}^n`.
+    The vector type is an iterable object.
+
+    Supported Operations:
+
+    * element-wise vector addition
+    * element-wise vector subtraction
+    * element-wise vector multiplication
+    * element-wise vector division
+    * scalar-vector multiplication
+    * scalar-vector division
+    * vector-scalar exponentiation
+
+    .. code-block:: python
+
+        >>> import vector as v
+        >>> a = v.vector([1,2,3])
+        >>> b = v.vector([4,5,6])
+        >>> c = 5
+        >>> a+b
+        [5, 7, 9]
+        >>> a-b
+        [-3, -3, -3]
+        >>> a*b
+        [4, 10, 18]
+        >>> a/b
+        [0.25, 0.4, 0.5]
+        >>> a*c
+        [5, 10, 15]
+        >>> a/c
+        [0.2, 0.4, 0.6]
+        >>> a**c
+        [1, 32, 243]
+
     """
 
     contents: List[Real]
@@ -148,6 +181,7 @@ class vector:
             return self.contents[index]
         raise IndexError('invalid index')
 
+
 def norm(x:vector) -> float:
     """
     .. math::
@@ -161,6 +195,14 @@ def norm(x:vector) -> float:
 
     Returns:
         float: norm of a vector :math:`||\\vec{x}||`
+
+    .. code-block:: python
+
+        >>> import vector as v
+        >>> a = v.vector([2,1,-2])
+        >>> v.norm(a)
+        3.0
+    
     """    
     if type(x) is not vector:
         raise TypeError('type expects a vector')
@@ -178,12 +220,22 @@ def normalize(p:vector, q:vector) -> vector:
 
     Returns:
         vector: coordinates of normalized vector
+
+    .. code-block:: python
+
+        >>> import vector as v
+        >>> p = v.vector([2,1,-2])
+        >>> q = v.vector([4,-5,6])
+        >>> v.normalize(p,q)
+        [-0.19611613513818404, 0.5883484054145521, -0.7844645405527362]
+
     """    
     if type(p) is not vector or type(q) is not vector:
         raise TypeError('p and q are expected to be a vector')
     
     dist:vector = p-q
     return (dist)/norm(dist)
+
 
 def dot(p:vector, q:vector) -> float:
     """dot product operation of two vectors
@@ -200,10 +252,20 @@ def dot(p:vector, q:vector) -> float:
 
     Returns:
         float: dot product of vector p and q 
+    
+    .. code-block:: python
+
+        >>> import vector as v
+        >>> a = v.vector([1,2,3])
+        >>> b = v.vector([4,-5,6])
+        >>> v.dot(a,b)
+        12
+
     """    
     if len(p) != len(q):
         raise ValueError('lengths of vectors must be equal')
     return sum(x0*x1 for (x0,x1) in zip(p,q))
+
 
 def cross(p:vector, q:vector) -> vector:
     """
@@ -217,10 +279,19 @@ def cross(p:vector, q:vector) -> vector:
         q (vector): represents coordinates oF vector q
 
     Raises:
-        NotImplementedError: [description]
+        NotImplementedError: when the dimensionality is not in :math:`\\mathbb{R}^3`
 
     Returns:
-        vector: [description]
+        vector: cross product of vectors in :math:`\\mathbb{R}^3`
+
+    ... code-block:: python
+
+        >>> import vector as v
+        >>> ab = v.vector([2,-1,6])
+        >>> bc = v.vector([0,2,-4])
+        >>> v.cross(ab,ac)
+        [-8,8,4]
+
     """    
     if len(p) != 3 and len(q) != 3:
         raise NotImplementedError()
@@ -234,8 +305,8 @@ def cross(p:vector, q:vector) -> vector:
 
 def get_angle(p:vector, q:vector) -> float: 
     """
-    ..math::
-        \\theta = \\acos \\left( \\frac{\\vec{p} \\cdot \\vec{q}}{|| \\vec{p} || || \\vec{q} ||}\\right)
+    .. math::
+        \\theta = \\arccos \\left( \\frac{\\vec{p} \\cdot \\vec{q}}{|| \\vec{p} || || \\vec{q} ||}\\right)
 
     Args:
         p (vector): represents coordinates of vector p
@@ -243,8 +314,18 @@ def get_angle(p:vector, q:vector) -> float:
 
     Returns:
         float: angle between p and q
+    
+    .. code-block:: python
+
+        >>> import vector as v
+        >>> a = v.vector([3,-2,3])
+        >>> b = v.vector([1,-3,-3])
+        >>> v.get_angle(a,b)
+        1.5707963267948966 # which is equal to π/2
+
     """    
     return acos(dot(p,q)/(norm(p)*norm(q)))
+
 
 def get_projection(p:vector, q:vector) -> vector: 
     """
@@ -257,8 +338,18 @@ def get_projection(p:vector, q:vector) -> vector:
 
     Returns:
         vector: coordinates of the projection of p to q
+    
+    .. code-block:: python
+
+        >>> import vector as v
+        >>> a = v.vector([2,3,5])
+        >>> b = v.vector([2,-1,-2])
+        >>> v.get_projection(b,a)
+        [-2.0, 1.0, 2.0]
+
     """    
     return p * (dot(p,q)/norm(p)**2 )
+
 
 def dist(p:vector, q:vector) -> vector:
     """
@@ -271,12 +362,76 @@ def dist(p:vector, q:vector) -> vector:
 
     Returns:
         vector: returns the distance of p to q
+
+    .. code-block:: python
+
+        >>> import vector as v
+        >>> x = v.vector([1,2,3])
+        >>> y = v.vector([4,5,6])
+        >>> v.dist(x,y)
+        [-3,-3,-3]
+
     """    
     if type(p) is not vector or type(q) is not vector:
         raise TypeError('p and q are expected to be of type vector')    
     return p - q
 
-def vectorize() -> vector: ...
+
+def vectorize(func:Callable, args:List[vector]) -> vector: 
+    """maps the values of a list of vector into a mathematical function
+
+    .. math::
+        f: \\langle \\vec{x_1}, ..., \\vec{x_n} \\rangle \\mapsto \\langle y_1, ... y_n \\rangle
+
+    Args:
+        func (Callable): a mathematical function that returns a value
+        args (List[vector]): list of vectors that are mapped as arguments
+
+    Raises:
+        RuntimeError: when the function expects a number of arguments not equal to the number of vectors
+
+    Returns:
+        vector: evaluation of :math:`f(\\vec{x_1}, ..., \\vec{x_n})`
+
+    .. code-block:: python
+
+        >>> import vector as v
+        >>> x = v.vector([1,2,3])
+        >>> y = v.vector([4,5,6])
+        >>> z = v.vector([7,8,9])
+        >>> v.vectorize(lambda  x,y,z: x+y+z, [x,y,z])
+        [12,15,18]
+
+    """    
+    if func.__code__.co_argcount != len(args):
+        raise RuntimeError('invalid expression: func is expected to take the same number'
+                            ' of arguments as the length of args')
+    return vector(list(map(func, *args)))
+
+
+def parameterize(functions:List[Callable], arg:Real) -> vector:
+    """parameterized vector which maps a single-valued argument into a list of functions
+
+    .. math::
+        \\vec{f}(x) = \\langle f_1(x), ... f_n(x) \\rangle
+
+    Args:
+        functions (List[Callable]): represents a list of mathematical functions that return a value
+        arg (Real): represents the single-valued argument
+
+    Returns:
+        vector: coordinates of a vector after evaluating the list of functions :math:`\\vec{f}(x)`.
+    
+    .. code-block:: python
+
+        >>> import vector as v
+        >>> from math import sin, cos, pi
+        >>> functions = [lambda x: cos(x), lambda x: sin(x*pi), lambda x: x*pi]
+        >>> v.parameterize(functions, pi)
+        [-1.0, -0.4303012170000917, 9.869604401089358]
+
+    """    
+    return vector([f(arg) for f in functions])
 
 if __name__ == '__main__':
     # v0 = vector([1,2,4,5])
